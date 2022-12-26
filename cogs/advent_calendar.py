183def139883590c3abc32ac90dc2c0080b4df35
Kesuaheli <kesuaheligame@gmail.com>
2022-12-26 20:36:51 +0100

    clean: type def

--- cogs/advent_calendar.py
+++ cogs/advent_calendar.py
@@ -1,32 +1,25 @@
-from files import sql as SQL
 from os import path
-from urllib.request import urlopen
-import sys
 import random
 import json
 
 from bot import Bot
-
-from discord.ext.commands import Cog
-from discord import Embed, RawReactionActionEvent, HTTPException
-from discord_slash import cog_ext, SlashContext
-from discord_slash.utils.manage_commands import create_choice, create_option
-
 from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime, date
 
-months = [("Januar", 31), ("Februar", 29), ("März", 31), ("April", 30), ("Mai", 31), ("Juni", 30),
-          ("Juli", 31), ("August", 31), ("September", 30), ("Oktober", 31), ("November", 30), ("Dezember", 31)]
+from discord.ext.commands import Cog
+from discord import Embed, RawReactionActionEvent, HTTPException, Member
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option
+
 with open(path.join(path.dirname(path.dirname(__file__)), "config.json")) as f:
     guild_id = json.load(f)['base']['guild_id']
 
 
 class AdventCalendar(Cog):
     def __init__(self, bot: Bot):
-        self.bot = bot
-        self.advent_channel = self.bot.get_channel(1046802329241931806)
-        self.today = date.today()
-        self.entries = {}
+        self.bot: Bot = bot
+        self.today: date = date.today()
+        self.entries: dict[int, int] = {}
 
     async def openCalendar(self):
         self.today = date.today()
@@ -48,7 +41,6 @@ class AdventCalendar(Cog):
 
         self.bot.log.info(f"Advent Calendar: Door {self.today.day}")
         image_url = f"https://cake4everyone.de/files/xmas/{self.today.day}.png"
-        self.bot.log.info("image: "+image_url)
 
         adventEmbed = Embed(
             description=msg,
@@ -60,11 +52,11 @@ class AdventCalendar(Cog):
         self.last_msg = await self.bot.advent_channel.send(embed=adventEmbed)
         await self.last_msg.add_reaction("🎅")
 
-    def nextDate(self, day: int, month: int):
-        original_day, original_month = day, month
+    def nextDate(self, day: int, month: int) -> date:
+        original_day, original_month: int = day, month
         if self.today.year % 4 and day == 29 and month == 2:
             day, month = 1, 3
-        next_date = date(self.today.year, month, day)
+        next_date: date = date(self.today.year, month, day)
 
         if next_date < self.today:
             if (self.today.year+1) % 4 and original_day == 29 and original_month == 2:
@@ -73,28 +65,31 @@ class AdventCalendar(Cog):
 
         return next_date
 
-    def getTotalTickets(self):
+    def getTotalTickets(self) -> int:
         total: int = 0
         for id, tickets in self.entries.items():
             if id == None or id <= 0:
-                self.bot.log.warn("skipping false entry: no valid ID for this entry")
+                self.bot.log.warn(
+                    "skipping false entry: no valid ID for this entry")
                 continue
             if tickets == None or tickets <= 0:
-                self.bot.log.warn(f"skipping false entry: number of entries for '{id}' aren't valid")
+                self.bot.log.warn(
+                    f"skipping false entry: number of entries for '{id}' aren't valid")
                 continue
-            
+
             total += tickets
         return total
 
-    def getWinChance(self, tickets):
-        c = tickets/self.getTotalTickets()*100
+    def getWinChance(self, tickets: int) -> str:
+        c: float = tickets/self.getTotalTickets()*100
         return f"{c:.3f}%"
 
     @Cog.listener()
     async def on_ready(self):
 
         # misfire_grace_time=(15 hours, 59 minutes, 58 seconds) to run latest at 23:59:58 on that day
-        self.bot.scheduler.add_job(self.openCalendar, CronTrigger(hour=8), misfire_grace_time=57598)
+        self.bot.scheduler.add_job(self.openCalendar, CronTrigger(
+            hour=8), misfire_grace_time=57598)
 
         self.bot.cogs_ready.ready_up(path.basename(__file__)[:-3])
 
@@ -118,18 +113,20 @@ class AdventCalendar(Cog):
         entriesEmbed = Embed(
             description=f"Es sind momentan folgende Leute eingetragen:",
             color=0x2f3136)
-        entriesEmbed.add_field(name="Übersicht", value=f"`Teilnehmer`: {len(self.entries)}\n`Tickets   `: {self.getTotalTickets()}\n`%/Ticket  `: {self.getWinChance(1)}", inline=False)
+        entriesEmbed.add_field(
+            name="Übersicht", value=f"`Teilnehmer`: {len(self.entries)}\n`Tickets   `: {self.getTotalTickets()}\n`%/Ticket  `: {self.getWinChance(1)}", inline=False)
 
-        await ctx.defer() # getting all usernames could take a bit
+        await ctx.defer()  # getting all usernames could take a bit
 
         for id, tickets in self.entries.items():
-            username = "could not get name"
             try:
-                user = await self.bot.guild.fetch_member(id)
+                user: Member = await self.bot.guild.fetch_member(id)
                 username = user.display_name
             except HTTPException as e:
                 self.bot.log.warn(e)
-            entriesEmbed.add_field(name=id, value=f"`Name   `: {username}\n`Tickets`: {tickets}\n`Chance `: {self.getWinChance(tickets)}")
+                username = "could not get name"
+            entriesEmbed.add_field(
+                name=id, value=f"`Name   `: {username}\n`Tickets`: {tickets}\n`Chance `: {self.getWinChance(tickets)}")
 
         entriesEmbed.set_footer(
             text=f"{self.bot.user.display_name} > Adventskalender", icon_url=self.bot.user.avatar_url)
@@ -145,44 +142,47 @@ class AdventCalendar(Cog):
             await ctx.send("No entries yet", hidden=True)
             return
 
-        winnerKey = random.randint(1,self.getTotalTickets())
-        winnerID: int = 0
-        winnerTickets: int = 0
-
-        counter: int = winnerKey
+        winnerKey: int = random.randint(1, self.getTotalTickets())
+        winnerID, winnerTickets: int = 0
         for id, tickets in self.entries.items():
             if id == None or id <= 0:
-                self.bot.log.warn("skipping false entry: no valid ID for this entry")
+                self.bot.log.warn(
+                    "skipping false entry: no valid ID for this entry")
                 continue
             if tickets == None or tickets <= 0:
-                self.bot.log.warn(f"skipping false entry: number of entries for '{id}' aren't valid")
+                self.bot.log.warn(
+                    f"skipping false entry: number of entries for '{id}' aren't valid")
                 continue
-            
-            counter -= tickets
-            if counter <= 0:
+
+            winnerKey -= tickets
+            if winnerKey <= 0:
                 winnerID = id
                 winnerTickets = tickets
                 break
 
-        #winner = None
         try:
-            winner = await self.bot.guild.fetch_member(winnerID)
+            winner: Member = await self.bot.guild.fetch_member(winnerID)
         except HTTPException as e:
             self.bot.log.warn(e)
             await ctx.send("Something went wrong...")
             return
-        
+
         winnerEmbed = Embed(
             title="Cake4Everyone Gewinnauslosung",
             color=0xbddeec)
         winnerEmbed.set_thumbnail(url=winner.avatar_url)
-        winnerEmbed.add_field(name="Gewonnen hat...", value=winner.mention)
-        winnerEmbed.add_field(name="᲼᲼", value="Herzliche Glückwünsche und frohe Weihnachten ❤️")
-        winnerEmbed.add_field(name="Alle Tickets", value=self.getTotalTickets(), inline=False)
-        winnerEmbed.add_field(name="Erhaltene Tickets", value=f"{winnerTickets}/24")
-        winnerEmbed.add_field(name="Gewinnwahrscheinlichkeit", value=self.getWinChance(winnerTickets))
-        winnerEmbed.set_footer(
-            text=f"{self.bot.user.display_name} > Adventskalender", icon_url=self.bot.user.avatar_url)
+        winnerEmbed.add_field(name="Gewonnen hat...",
+                              value=winner.mention)
+        winnerEmbed.add_field(name="᲼᲼",
+                              value="Herzliche Glückwünsche und frohe Weihnachten ❤️")
+        winnerEmbed.add_field(name="Alle Tickets",
+                              value=self.getTotalTickets(), inline=False)
+        winnerEmbed.add_field(name="Erhaltene Tickets",
+                              value=f"{winnerTickets}/24")
+        winnerEmbed.add_field(name="Gewinnwahrscheinlichkeit",
+                              value=self.getWinChance(winnerTickets))
+        winnerEmbed.set_footer(text=f"{self.bot.user.display_name} > Adventskalender",
+                               icon_url=self.bot.user.avatar_url)
 
         await ctx.send(embed=winnerEmbed)
 
@@ -204,7 +204,7 @@ class AdventCalendar(Cog):
                                               max_value=24
                                               )
                             ])
-    async def addMember(self, ctx: SlashContext, person, tickets):
+    async def addMember(self, ctx: SlashContext, person: Member, tickets: int):
         if tickets == 0:
             del self.entries[person.id]
             await ctx.send(f"Der Eintrag von {person.display_name} wurde entfernt!")
@@ -228,7 +228,7 @@ class AdventCalendar(Cog):
             self.bot.log.debug(f"not correct emoji: '{e.emoji}'")
             return
 
-        reactUser = await self.bot.guild.fetch_member(e.user_id)
+        reactUser: Member = await self.bot.guild.fetch_member(e.user_id)
         if reactUser.bot:
             self.bot.log.debug("reactor is bot")
             return
@@ -236,7 +236,8 @@ class AdventCalendar(Cog):
         dTime = datetime.utcnow() - message.created_at
         timeFormat = f"{int(dTime.total_seconds())//3600}h, {int(dTime.total_seconds())%3600//60}m, {int(dTime.total_seconds())%60}s"
         under16hrs = ":white_check_mark:" if dTime.total_seconds() < 16*3600 else ":x:"
-        reaction = next(filter(lambda r: r.emoji == e.emoji.name or r.emoji.name == e.emoji.name, message.reactions), None)
+        reaction = next(filter(lambda r: r.emoji == e.emoji.name or r.emoji.name ==
+                        e.emoji.name, message.reactions), None)
 
         await self.bot.guild_log.send(
             f"new reaction from {reactUser.mention}: \n> `Type `: `{e.event_type}`\n> `Time `: {timeFormat}\n> `<16h `: {under16hrs}\n> `Count`: {reaction.count}")
